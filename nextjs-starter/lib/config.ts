24792b6c0d113f8ca7840b25bd21a2d91a2d062f
ptakondrej <florixakcz@seznam.cz>
2026-07-09 15:38:25 +0200

    refactor: use env util

--- nextjs-starter/lib/config.ts
+++ nextjs-starter/lib/config.ts
@@ -1,10 +1,12 @@
+import { env } from "./env";
+
 export const siteConfig = {
   name: "My App",
   description: "A production-ready Next.js application.",
-  url: process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000",
+  url: env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000",
   links: {
     github: "https://github.com/florixak",
   },
-} as const
+} as const;
 
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
